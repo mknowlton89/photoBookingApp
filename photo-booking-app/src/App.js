3c307a60165d44fc780bae28a14568ccd3f0cfee
Michael Knowlton <mknowlton89@gmail.com>
2021-05-12 20:41:10 -0400

    Installed react-router-dom and built some initial routes. Built out the /book screen with a multi-step form where a user can select a time slot, enter their details, and get a confirmation screen. I still need to add validation and error handling to the form, and I need to figure out how to limit the users to only selecting a single appointment slot.

--- photo-booking-app/src/App.js
+++ photo-booking-app/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { BookingContainer } from './components/BookingContainer/BookingContainer';
+import { BookAppointment } from './screens/BookAppointment/BookAppointment';
+import { Dashboard } from './screens/Dashboard/Dashboard';
+import { Login } from './screens/Login/Login';
+import { Signup } from './screens/Signup/Signup';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <BookingContainer />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/book">
+          <BookAppointment />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
